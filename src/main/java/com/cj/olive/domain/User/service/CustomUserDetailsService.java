dc5399c1f0813521b2ad424ec28976896fbd3545
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-13 08:08:31 +0900

    feat: jwt를 통한 로그인 api 추가(#3)

---
+++ src/main/java/com/cj/olive/domain/User/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.cj.olive.domain.User.service;
+
+import com.cj.olive.domain.User.model.CustomUserDetails;
+import com.cj.olive.domain.User.entity.User;
+import com.cj.olive.domain.User.error.UserErrorCode;
+import com.cj.olive.domain.User.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(UserErrorCode.LOGIN_FAILED.getMessage()));
+        return new CustomUserDetails(user);
+    }
+}
