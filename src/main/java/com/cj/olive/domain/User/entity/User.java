ea87e881620fac85d0077d262342546ed0e710c5
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-07-13 08:03:37 +0900

    feat: user 엔티티 추가(#3)

---
+++ src/main/java/com/cj/olive/domain/User/entity/User.java
@@ -0,0 +1,69 @@
+package com.cj.olive.domain.User.entity;
+
+import com.cj.olive.domain.User.model.UserTypeEnum;
+import com.cj.olive.global.entity.BaseTime;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@DynamicUpdate
+public class User extends BaseTime {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String phoneNumber;
+
+    private String deviceToken;
+
+    @Enumerated(EnumType.STRING)
+    private UserTypeEnum userType;
+
+    @Builder
+    public User(@NotNull String username, String nickname, String password, String phoneNumber, UserTypeEnum userType) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.userType = userType;
+    }
+
+    public UserTypeEnum updateUserType(UserTypeEnum userTypeEnum) {
+        this.userType = userTypeEnum;
+        return this.userType;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof User user)) return false;
+
+        return Objects.equals(this.id, user.getId()) &&
+                Objects.equals(this.username, user.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+}
---
+++ src/main/java/com/cj/olive/domain/User/model/UserTypeEnum.java
@@ -0,0 +1,5 @@
+package com.cj.olive.domain.User.model;
+
+public enum UserTypeEnum {
+    USER, ADMIN
+}
